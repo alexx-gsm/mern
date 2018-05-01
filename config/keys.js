08e5d929379eadf079e9c4da98aee595f9c0abb6
alexx-gsm <alexx-gsm@yandex.ru>
2018-05-02 01:33:56 +0700

    server: user, profile, post

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://alexx:25L10i1979@ds161939.mlab.com:61939/vkda"
+  mongoURI: 'mongodb://alexx:25L10i1979@ds161939.mlab.com:61939/vkda',
+  secretOrKey: 'secret'
 };
